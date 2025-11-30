d02030d3ce53265dd583c36d1777021581816a8b
kasarshravani04 <shravanikasar05@gmail.com>
2025-12-01 00:00:17 +0530

    added logout and delete todo

---
+++ To_Do/script/Todo.js
@@ -0,0 +1,12 @@
+class Todo {
+    constructor(title,desc,priority,deadline){
+        this.title = title;
+         this.desc = desc;
+    this.priority = priority;
+    this.deadline = deadline;
+    this.completed = false;
+  }
+}
+
+export default Todo;
+    
